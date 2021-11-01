7d55f3a560cfd6bdc0dea5e294efe4dea5cb2d4d
Gary Hawes <gary.hawes@gmail.com>
2021-11-01 17:02:13 +0000

    Setup MikroOrm with Postgres

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  orm.getMigrator().up()
+  // const post = orm.em.create(Post, { title: "My first post" });
+  // await orm.em.persistAndFlush(post)
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts)
+}
+
+
+main().catch(err => {
+  console.error("Error:" + err)
+});
