73ad58881e975b12c892a11a23bf5d249b5ebddc
Gary Hawes <gary.hawes@gmail.com>
2023-05-09 21:39:40 +0100

    Stuck on typescript warning

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [],
-    dbName: "lireddit",
-    type: "postgresql",
-    debug: !__prod__,
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().createMigration();
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, {
+    title: "my first post",
   });
+  await orm.em.persistAndFlush(post);
 };
 
-main();
+main().catch((err) => {
+  console.error(err);
+});
