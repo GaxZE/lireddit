ae857b596b5b877fc0959caea7fee7e97173f1d9
Gary Hawes <gary.hawes@gmail.com>
2023-05-09 16:10:23 +0100

    Missing comma

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { __prod__ } from "./constants";
 
 const main = async () => {
   const orm = await MikroORM.init({
+    entities: [],
     dbName: "lireddit",
     type: "postgresql",
     debug: !__prod__,
