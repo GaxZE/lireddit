6ab67ec2583ff434884226638e2c1e2e38aaf436
Gary Hawes <gary.hawes@gmail.com>
2023-05-10 11:49:33 +0100

    Getting posts from db and serving through graphql

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(5000, () => {
+    console.log("Server started on http://localhost:5000/graphql");
+  });
 };
 
 main().catch((err) => {
