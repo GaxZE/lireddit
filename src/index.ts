7dfd184ffe52474ccf2a72a2aa91862c9d5ced23
Gary Hawes <gary.hawes@gmail.com>
2023-05-10 11:20:33 +0100

    Adding graphql, express

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,6 @@ import mikroOrmConfig from "./mikro-orm.config";
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, {
-  //   title: "my first post",
-  // });
-  // await orm.em.persistAndFlush(post);
-
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
 };
 
 main().catch((err) => {
